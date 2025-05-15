6d470328bfd3965e8e0b6043aa0c04508339e311
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2025-05-15 17:08:53 +0200

    feat: add react toolkit query to the project

--- src/pages/HeroDetails.tsx
+++ src/pages/HeroDetails.tsx
@@ -3,13 +3,15 @@ import { useNavigate, useParams } from 'react-router';
 import { getHeroById } from '../api/heroes';
 import HeroCard from '../components/HeroCard';
 import HeroSkeletonCard from '../components/skeletons/HeroCardSkeleton';
+import { useGetHeroByIdQuery } from '@/redux/services/heroes';
 
 const HeroDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  const { data, isError, isFetching, isLoading, isSuccess  } = useQuery({ queryKey: ['hero', id], queryFn: () => getHeroById(id!) })
+  // const { data, isError, isFetching, isLoading, isSuccess } = useQuery({ queryKey: ['hero', id], queryFn: () => getHeroById(id!) })
   // hero, 2 -> Cache isLoading = false, isFetching = true
+  const { data, isLoading, isError, isSuccess, isFetching } = useGetHeroByIdQuery(id!);
 
   return (
     <section>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import favoriteHeroesReducer from './slices/favoritesSlice'
 import fightHistoryReducer from './slices/fightHistorySlice'
+import { heroesApi } from './services/heroes'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     favoriteHeroes: favoriteHeroesReducer,
     fightHistory: fightHistoryReducer,
+    [heroesApi.reducerPath]: heroesApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(heroesApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
